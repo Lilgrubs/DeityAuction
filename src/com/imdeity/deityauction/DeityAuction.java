da8a0acdbd26d693b86d3b06b98b8955ac970093
mbonachea <bonacheam@gmail.com>
2013-02-06 18:29:38 -0500

    inital commit

---
+++ src/com/imdeity/deityauction/DeityAuction.java
@@ -0,0 +1,14 @@
+package com.imdeity.deityauction;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeityAuction extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
