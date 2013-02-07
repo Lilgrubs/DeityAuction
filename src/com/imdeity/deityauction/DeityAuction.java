43cdf0850514a4388d9c6b2ce7827dd185b2370d
mbonachea <bonacheam@gmail.com>
2013-02-06 19:46:06 -0500

    added logger

--- src/com/imdeity/deityauction/DeityAuction.java
+++ src/com/imdeity/deityauction/DeityAuction.java
@@ -1,14 +1,17 @@
 package com.imdeity.deityauction;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DeityAuction extends JavaPlugin {
 	
+	Logger logger = Logger.getLogger("Minecraft");
 	public void onEnable() {
-		
+		logger.info("[DeityAuction] DeityAuction is now enabled!");
 	}
 	
 	public void onDisable() {
-		
+		logger.info("[DeityAuction] DeityAuction is now disabled!");
 	}
 }
